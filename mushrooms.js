9e8c6ee2fe32a82633f2272c8df311e3822ad6a1
Valeentin87 <valeentin87@list.ru>
2024-06-09 13:00:33 +0300

    добавил на страницу контент со съедобными грибами (без стилистики)

--- mushrooms.js
+++ mushrooms.js
@@ -1,77 +1,77 @@
-{
+const mushData = `{
   "edible_mushrooms": [
     {
-      "Белый гриб": {
+      
         "name": "Белый гриб",
         "description": "Белый гриб – один из наиболее ценных грибов, растущих в наших широтах. Да чего уж там… Просто самый ценный! Многие грибники вообще не признают никаких грибов, кроме белых.     ",
         "mushroom_image": "http://griby.org.ua/wp-content/uploads/belyi-grib-opisanie-150x150.jpg"
-      }
+      
     },
     {
-      "Вешенки": {
+      
         "name": "Вешенки",
         "description": "это как широко известная вешенка устричная, так и несколько видов других, менее известных разновидностей вешенок.     ",
         "mushroom_image": "http://griby.org.ua/wp-content/uploads/veshenki_opisanie-150x150.jpg"
-      }
+      
     },
     {
-      "Дождевики": {
+      
         "name": "Дождевики",
         "description": "Дождевики – своеобразные грибы, в форме шара или лампочки. Многие съедобны в молодом возрасте.    ",
         "mushroom_image": "http://griby.org.ua/wp-content/uploads/dozhdeviki-opisanie-150x150.jpg"
-      }
+      
     },
     {
-      "Зонтики": {
+     
         "name": "Зонтики",
         "description": "Кто-то обожает  зонтики и встречает радостным криком, а кто-то – шарахается, как от поганки. Но никто не останется равнодушным, встретив этот замечательный гриб-великан.    ",
         "mushroom_image": "http://griby.org.ua/wp-content/uploads/zontik.jpg"
-      }
+      
     },
     {
-      "Иудино ухо": {
+      
         "name": "Иудино ухо",
         "description": "экзотический гриб, более характерный для азиатской кухни, но произрастающий также и в наших широтах.    ",
         "mushroom_image": "http://griby.org.ua/wp-content/uploads/iudino-uho-opisanie-150x150.jpg"
-      }
+      
     },
     {
-      "Лисички": {
+      
         "name": "Лисички",
         "description": "съедобные и вкусные грибы. Произрастают в смешанных и лиственных лесах, с июня по октябрь. Все виды приготовления, лёгкость в обработке &#8212; достоинства лисичек.    ",
         "mushroom_image": "http://griby.org.ua/wp-content/uploads/lisichka-opisanie-150x150.jpg"
-      }
+      
     }
   ],
 
   "poisonous_mushrooms": [
     {
-      "Бледная поганка": {
+      
         "name": "Бледная поганка",
         "description": "смертельно ядовитый и очень опасный гриб. Произрастает летом и осенью в лиственных и смешанных лесах.     ",
         "mushroom_image": "http://griby.org.ua/wp-content/uploads/blednaya-poganka-opisanie-150x150.jpg"
-      }
+    
     },
     {
-      "Мухомор пантерный": {
+     
         "name": "Мухомор пантерный",
         "description": "ядовитый гриб, по опасности уступающий лишь бледной поганке. Избегайте таких грибов.     ",
         "mushroom_image": "http://griby.org.ua/wp-content/uploads/muhomor-panternyj-opisanie-150x150.jpg"
-      }
+      
     },
     {
-      "Свинушка тонкая": {
+     
         "name": "Свинушка тонкая",
         "description": "Свинушка тонкая – ядовитый гриб, хотя долгое время считался съедобным. Длительное употребление в пищу приводит к отравлениям, иногда со смертельным исходом.     ",
         "mushroom_image": "http://griby.org.ua/wp-content/uploads/svinushka-tonkaja-opisanie-150x150.jpg"
-      }
+     
     },
     {
-      "Энтолома весенняя": {
+     
         "name": "Энтолома весенняя",
         "description": "ядовитый гриб, произрастающий в апреле-мае в лиственных лесах.     ",
         "mushroom_image": "http://griby.org.ua/wp-content/uploads/entoloma-vesennyaya-opisanie-150x150.jpg"
-      }
+      
     }
   ]
-}
+}`
